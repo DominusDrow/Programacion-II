72555dfc91a6f5072afd40252d3d7cb6f787298b
avalo <avalo@192.168.1.69>
2020-10-28 20:37:20 -0600

    miavionputop

--- Actividad9/src/mediosTransporte/avion.java
+++ Actividad9/src/mediosTransporte/avion.java
@@ -4,21 +4,39 @@ import java.util.*;
 
 public class avion extends transporte_aereo implements Comunicacion {
 
-    private int motores;
-    private int azafatas;
+    private int numVuelo;
+    private String aerolinea;
 
-    public avion() {
+    public avion(String aero, int numVuelo) {
+    	this.aerolinea=aero;
+    	this.numVuelo=numVuelo;
     }
 
+    public void setNumvuelo(int num) {
+    	numVuelo=num;
+    }
+    
+    public int getNumvuelo() {
+    	return numVuelo;
+    }
+    
+    public void setAero(String aero) {
+    	aerolinea=aero;
+    }
+    
+    public String getAero() {
+    	return aerolinea;
+    }
+    
 
     public String comunicaTorreControl() {
       
-    	return"";
+    	return"Heathrow Control, "+aerolinea+" Vuelo "+numVuelo+", mi mensaje es como sigue: Mayday, Mayday, Mayday ....";
     }
 
     public String msgHumano() {
         
-    	return "";
+    	return "Hola, soy tu avion, a donde quieres ir";
     }
 
 
