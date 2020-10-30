527e305893cb9996055360140c0ee895a435d551
avalo <avalo@192.168.1.69>
2020-10-29 22:50:21 -0600

    si

--- Actividad9/src/mediosTransporte/Test_MediosTransporte.java
+++ Actividad9/src/mediosTransporte/Test_MediosTransporte.java
@@ -4,7 +4,14 @@ public class Test_MediosTransporte {
 
 	public static void main(String[] args) {
 		
+		avion av1=new avion("Boeing", 400, 1700,"Interjet",1155);
+		naveEspacial nv1=new naveEspacial(2,true);
 		
+		System.out.println(av1.Despegar());
+	
+		System.out.println(nv1.Despegar());
+
+
 		
 		
 		
